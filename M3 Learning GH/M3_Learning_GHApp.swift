248ece691c797192a19d9d543b6a9ecb20aac0a9
Zach <z.cuneo03@gmail.com>
2022-12-13 01:22:35 -0700

    Initial Commit

---
+++ M3 Learning GH/M3_Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  M3_Learning_GHApp.swift
+//  M3 Learning GH
+//
+//  Created by Account on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct M3_Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

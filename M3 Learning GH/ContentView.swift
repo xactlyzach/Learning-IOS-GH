30c9c51f3a3ea4a030e2dbb1f8e8fe1efcb426ad
Zachary Cuneo <78629978+xactlyzach@users.noreply.github.com>
2022-12-13 02:39:12 -0700

    Update ContentView to What's up!

--- M3 Learning GH/ContentView.swift
+++ M3 Learning GH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, CodeCrew!")
+            Text("What's up, CodeCrew!")
         }
         .padding()
     }
